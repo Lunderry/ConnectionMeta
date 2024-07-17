0de4d5d37f16c8340869d373e9b9768b5c81a8cd
Lunderry <115415867+Lunderry@users.noreply.github.com>
2024-07-17 12:06:23 -0600

    Fix: Error to type

--- src/init.lua
+++ src/init.lua
@@ -3,7 +3,7 @@
 export type MetaConnection<T> = {
 	pack: any,
 	Add: (self: MetaConnection<T>, ...T) -> ...T | T,
-	Disconnect: (self: MetaConnection<T>, q: number | T) -> (),
+	Disconnect: (self: MetaConnection<T>, q: number | T?) -> (),
 	Destroy: (self: MetaConnection<T>) -> (),
 	Unpack: (self: MetaConnection<T>) -> ...T,
 }
